36359b389c39f99c3012331d62f989c1f6b1cba7
imjayapple <imjayapple@gmail.com>
2023-04-02 21:14:52 -0700

    Logic for the program, try-except block to prompt the user to input a valid response, call the function

--- multiplication.py
+++ multiplication.py
@@ -6,3 +6,23 @@ def display_multiplication_table(number):
     print(f"\nMultiplication table for {number}:\n")
     for i in range(1, 11):
         print(f"{number} x {i} = {number * i}")
+
+
+# Create the logic for the program
+# While loop to prompt the user to input a valid value, try-except block
+# If valid, call the display_multi.... function, passing the valid integer 'number' as an argument
+
+def main():
+    print("Welcome to the Multiplication Table Generator!")
+
+    while True:
+        try:
+            number = int(input("\nEnter a number for which you want to generate a multiplication table: "))
+            break
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
+    display_multiplication_table(number)
+
+if __name__ == "__main__":
+    main()
